7db1a1a135c2769fc1f9873210be17f1e8d1e1af
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-25 17:45:30 -0400

    write construct note

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(message, letters) {
+	if (letters.length < message.length) return false;
+	const msg = {};
+	const ltrs = {};
+	for (let char of letters) {
+		ltrs[char] = ltrs[char] + 1 || 1;
+	}
+	for (let char of message) {
+		msg[char] = msg[char] + 1 || 1;
+	}
+	for (let char in msg) {
+		if (!ltrs[char] || msg[char] > ltrs[char]) return false;
+	}
+	return true;
 }
