233beff5bb9960f8284ba55a4ad9696476e2a9e4
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-25 17:58:40 -0400

    write average pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,19 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(sortedArr, val) {
+	if (sortedArr.length <= 1) return false;
+
+	let left = 0;
+	let right = sortedArr.length - 1;
+
+	while (left < right) {
+		let avg = (sortedArr[left] + sortedArr[right]) / 2;
+		if (avg === val) {
+			return true;
+		} else if (avg > val) {
+			right--;
+		} else {
+			left++;
+		}
+	}
+	return false;
 }
