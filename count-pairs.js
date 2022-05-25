ff7be2a787265a08a48be194ec4f85c86f917766
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-25 19:11:38 -0400

    finish exercise

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(nums, val) {
+	let count = 0;
+	nums.sort((a, b) => a - b);
+
+	left = 0;
+	right = nums.length - 1;
+
+	while (left < right) {
+		let sum = nums[left] + nums[right];
+
+		if (sum === val) {
+			count++;
+			left++;
+		} else if (sum < val) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+
+	return count;
 }
