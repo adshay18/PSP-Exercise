e8942da9aa9ae0b6ef071d9e1d30b80336519718
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-25 18:36:46 -0400

    write seppositive

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(ints) {
+	let left = 0;
+	let right = ints.length - 1;
+	while (left < right) {
+		let temp1 = ints[left];
+		let temp2 = ints[right];
+		if (ints[left] < 0) {
+			ints.splice(left, 1);
+			ints.push(temp1);
+			right--;
+		} else if (ints[right] > 0) {
+			ints.splice(right, 1);
+			ints.unshift(temp2);
+			left++;
+		} else {
+			left++;
+			right--;
+		}
+	}
+	return ints;
 }
